2977cc2d83b222bf8412cb25a1799a3c3c39d829
notchum <45953494+notchum@users.noreply.github.com>
2026-02-21 06:36:47 -0500

    added some useful utils (#24)
    Co-authored-by: notchum <notchum@duck.com>

--- fluxer/utils.py
+++ fluxer/utils.py
@@ -1,6 +1,11 @@
 from __future__ import annotations
 
+import os
+import pkgutil
+import re
+from collections.abc import Iterator
 from datetime import datetime, timezone
+from typing import Literal
 
 # Fluxer uses the same epoch as Discord: 2015-01-01T00:00:00Z
 FLUXER_EPOCH = 1420070400000
@@ -36,3 +41,196 @@ def datetime_to_snowflake(dt: datetime) -> int:
     timestamp_ms = int(dt.timestamp() * 1000)
     snowflake = (timestamp_ms - FLUXER_EPOCH) << 22
     return snowflake
+
+
+def utcnow() -> datetime:
+    """A helper function to return an aware UTC datetime representing the current time.
+
+    This should be preferred to :meth:`datetime.datetime.utcnow` since it is an aware
+    datetime, compared to the naive datetime in the standard library.
+
+    Returns
+    -------
+    :class:`datetime.datetime`
+        The current aware datetime in UTC.
+    """
+    return datetime.now(timezone.utc)
+
+
+_MARKDOWN_ESCAPE_SUBREGEX = "|".join(
+    rf"\{c}(?=([\s\S]*((?<!\{c})\{c})))" for c in ("*", "`", "_", "~", "|")
+)
+
+_MARKDOWN_ESCAPE_COMMON = r"^>(?:>>)?\s|\[.+\]\(.+\)"
+
+_MARKDOWN_ESCAPE_REGEX = re.compile(
+    rf"(?P<markdown>{_MARKDOWN_ESCAPE_SUBREGEX}|{_MARKDOWN_ESCAPE_COMMON})", re.MULTILINE
+)
+
+_URL_REGEX = r"(?P<url><[^: >]+:\/[^ >]+>|(?:https?|steam):\/\/[^\s<]+[^<.,:;\"\'\]\s])"
+
+_MARKDOWN_STOCK_REGEX = rf"(?P<markdown>[_\\~|\*`]|{_MARKDOWN_ESCAPE_COMMON})"
+
+
+def remove_markdown(text: str, *, ignore_links: bool = True) -> str:
+    """A helper function that removes markdown characters.
+
+    .. note::
+            This function is not markdown aware and may remove meaning from the original text. For example,
+            if the input contains ``10 * 5`` then it will be converted into ``10  5``.
+
+    Parameters
+    ----------
+    text: :class:`str`
+        The text to remove markdown from.
+    ignore_links: :class:`bool`
+        Whether to leave links alone when removing markdown. For example,
+        if a URL in the text contains characters such as ``_`` then it will
+        be left alone. Defaults to ``True``.
+
+    Returns
+    -------
+    :class:`str`
+        The text with the markdown special characters removed.
+    """
+
+    def replacement(match: re.Match) -> str:
+        groupdict = match.groupdict()
+        return groupdict.get("url", "")
+
+    regex = _MARKDOWN_STOCK_REGEX
+    if ignore_links:
+        regex = f"(?:{_URL_REGEX}|{regex})"
+    return re.sub(regex, replacement, text, flags=re.MULTILINE)
+
+
+def escape_markdown(text: str, *, as_needed: bool = False, ignore_links: bool = True) -> str:
+    r"""A helper function that escapes Fluxer's markdown.
+
+    Parameters
+    ----------
+    text: :class:`str`
+        The text to escape markdown from.
+    as_needed: :class:`bool`
+        Whether to escape the markdown characters as needed. This
+        means that it does not escape extraneous characters if it's
+        not necessary, e.g. ``**hello**`` is escaped into ``\*\*hello**``
+        instead of ``\*\*hello\*\*``. Note however that this can open
+        you up to some clever syntax abuse. Defaults to ``False``.
+    ignore_links: :class:`bool`
+        Whether to leave links alone when escaping markdown. For example,
+        if a URL in the text contains characters such as ``_`` then it will
+        be left alone. This option is not supported with ``as_needed``.
+        Defaults to ``True``.
+
+    Returns
+    -------
+    :class:`str`
+        The text with the markdown special characters escaped with a slash.
+    """
+    if not as_needed:
+
+        def replacement(match: re.Match) -> str:
+            groupdict = match.groupdict()
+            is_url = groupdict.get("url")
+            if is_url:
+                return is_url
+            return "\\" + groupdict["markdown"]
+
+        regex = _MARKDOWN_STOCK_REGEX
+        if ignore_links:
+            regex = f"(?:{_URL_REGEX}|{regex})"
+        return re.sub(regex, replacement, text, flags=re.MULTILINE)
+    else:
+        text = re.sub(r"\\", r"\\\\", text)
+        return _MARKDOWN_ESCAPE_REGEX.sub(r"\\\1", text)
+
+
+TimestampStyle = Literal["t", "T", "d", "D", "f", "F", "s", "S", "R"]
+
+
+def format_dt(dt: datetime | float, /, style: TimestampStyle = "f") -> str:
+    """Format a :class:`datetime.datetime`, :class:`int` or :class:`float` (seconds) for presentation within Fluxer.
+
+    This allows for a locale-independent way of presenting data using Fluxer specific Markdown.
+
+    +-------------+-------------------------------+------------------------+
+    |    Style    |        Example Output         |      Description       |
+    +=============+===============================+========================+
+    | t           | 22:57                         | Short Time             |
+    +-------------+-------------------------------+------------------------+
+    | T           | 22:57:58                      | Long Time              |
+    +-------------+-------------------------------+------------------------+
+    | d           | 17/05/2016                    | Short Date             |
+    +-------------+-------------------------------+------------------------+
+    | D           | 17 May 2016                   | Long Date              |
+    +-------------+-------------------------------+------------------------+
+    | f (default) | 17 May 2016 at 22:57          | Long Date, Short Time  |
+    +-------------+-------------------------------+------------------------+
+    | F           | Tuesday, 17 May 2016 at 22:57 | Full Date, Short Time  |
+    +-------------+-------------------------------+------------------------+
+    | s           | 17/05/2016, 22:57             | Short Date, Short Time |
+    +-------------+-------------------------------+------------------------+
+    | S           | 17/05/2016, 22:57:58          | Short Date, Long Time  |
+    +-------------+-------------------------------+------------------------+
+    | R           | 5 years ago                   | Relative Time          |
+    +-------------+-------------------------------+------------------------+
+
+    Note that the exact output depends on the user's locale setting in the client. The example output
+    presented is using the ``en-GB`` locale.
+
+    Parameters
+    ----------
+    dt: :class:`datetime.datetime` | :class:`int` | :class:`float`
+        The datetime to format.
+        If this is a naive datetime, it is assumed to be local time.
+    style: :class:`str`
+        The style to format the datetime with. Defaults to ``f``
+
+    Returns
+    -------
+    :class:`str`
+        The formatted string.
+    """
+    if isinstance(dt, datetime):
+        dt = dt.timestamp()
+    return f"<t:{int(dt)}:{style}>"
+
+
+def search_directory(path: str) -> Iterator[str]:
+    """Walk through a directory and yield all modules.
+
+    Parameters
+    ----------
+    path: :class:`str`
+        The path to search for modules
+
+    Yields
+    ------
+    :class:`str`
+        The name of the found module. (usable in load_extension)
+    """
+    relpath = os.path.relpath(path)  # relative and normalized
+    if ".." in relpath:
+        msg = "Modules outside the cwd require a package to be specified"
+        raise ValueError(msg)
+
+    abspath = os.path.abspath(path)
+    if not os.path.exists(relpath):
+        msg = f"Provided path '{abspath}' does not exist"
+        raise ValueError(msg)
+    if not os.path.isdir(relpath):
+        msg = f"Provided path '{abspath}' is not a directory"
+        raise ValueError(msg)
+
+    prefix = relpath.replace(os.sep, ".")
+    if prefix in ("", "."):
+        prefix = ""
+    else:
+        prefix += "."
+
+    for _, name, ispkg in pkgutil.iter_modules([path]):
+        if ispkg:
+            yield from search_directory(os.path.join(path, name))
+        else:
+            yield prefix + name
---
+++ tests/test_utils.py
@@ -0,0 +1,155 @@
+from __future__ import annotations
+
+import os
+from datetime import datetime, timezone
+
+import pytest
+
+from fluxer import utils
+
+
+@pytest.mark.parametrize(
+    ("num", "expected"),
+    [
+        (0, datetime(2015, 1, 1, tzinfo=timezone.utc)),
+        (881536165478499999, datetime(2021, 8, 29, 13, 50, 0, tzinfo=timezone.utc)),
+        (10000000000000000000, datetime(2090, 7, 20, 17, 49, 51, tzinfo=timezone.utc)),
+    ],
+)
+def test_snowflake_to_datetime(num: int, expected) -> None:
+    assert utils.snowflake_to_datetime(num).replace(microsecond=0) == expected
+
+
+@pytest.mark.parametrize(
+    ("dt", "expected"),
+    [
+        (datetime(2015, 1, 1, tzinfo=timezone.utc), 0),
+        (datetime(2021, 8, 29, 13, 50, 0, tzinfo=timezone.utc), 881536165478400000),
+    ],
+)
+def test_datetime_to_snowflake(dt, expected) -> None:
+    assert utils.datetime_to_snowflake(dt) == expected
+
+
+def test_utcnow() -> None:
+    assert utils.utcnow().tzinfo == timezone.utc
+
+
+@pytest.mark.parametrize(
+    ("text", "exp_remove", "exp_escape"),
+    [
+        (
+            # this is obviously not valid markdown for the most part,
+            # it's just meant to test several combinations
+            "*hi* ~~a~ |aaa~*\\``\n`py x``` __uwu__ y",
+            "hi a aaa\npy x uwu y",
+            r"\*hi\* \~\~a\~ \|aaa\~\*\\\`\`" + "\n" + r"\`py x\`\`\` \_\_uwu\_\_ y",
+        ),
+        (
+            "aaaaa\n> h\n>> abc \n>>> nay*ern_",
+            "aaaaa\nh\n>> abc \nnayern",
+            "aaaaa\n\\> h\n>> abc \n\\>>> nay\\*ern\\_",
+        ),
+        (
+            "*h*\n> [li|nk](~~url~~) xyz **https://google.com/stuff?uwu=owo",
+            "h\n xyz https://google.com/stuff?uwu=owo",
+            # NOTE: currently doesn't escape inside `[x](y)`, should that be changed?
+            r"\*h\*" + "\n" + r"\> \[li|nk](~~url~~) xyz \*\*https://google.com/stuff?uwu=owo",
+        ),
+    ],
+)
+def test_markdown(text: str, exp_remove, exp_escape) -> None:
+    assert utils.remove_markdown(text, ignore_links=False) == exp_remove
+    assert utils.remove_markdown(text, ignore_links=True) == exp_remove
+
+    assert utils.escape_markdown(text, ignore_links=False) == exp_escape
+    assert utils.escape_markdown(text, ignore_links=True) == exp_escape
+
+
+@pytest.mark.parametrize(
+    ("text", "expected", "expected_ignore"),
+    [
+        (
+            "http://google.com/~test/hi_test ~~a~~",
+            "http://google.com/test/hitest a",
+            "http://google.com/~test/hi_test a",
+        ),
+        (
+            "abc [link](http://test~test.com)\n>>> <http://endless.horse/_*>",
+            "abc \n<http://endless.horse/>",
+            "abc \n<http://endless.horse/_*>",
+        ),
+    ],
+)
+def test_markdown_links(text: str, expected, expected_ignore) -> None:
+    assert utils.remove_markdown(text, ignore_links=False) == expected
+    assert utils.remove_markdown(text, ignore_links=True) == expected_ignore
+
+
+@pytest.mark.parametrize(
+    ("dt", "style", "expected"),
+    [
+        (0, "F", "<t:0:F>"),
+        (1630245000.1234, "T", "<t:1630245000:T>"),
+        (datetime(2021, 8, 29, 13, 50, 0, tzinfo=timezone.utc), "f", "<t:1630245000:f>"),
+    ],
+)
+def test_format_dt(dt, style, expected) -> None:
+    assert utils.format_dt(dt, style) == expected
+
+
+@pytest.fixture(scope="session")
+def tmp_module_root(tmp_path_factory):
+    # this obviously isn't great code, but it'll do just fine for tests
+    tmpdir = tmp_path_factory.mktemp("module_root")
+    for d in ["empty", "not_a_module", "mod/sub1/sub2"]:
+        (tmpdir / d).mkdir(parents=True)
+    for f in [
+        "test.py",
+        "not_a_module/abc.py",
+        "mod/__init__.py",
+        "mod/ext.py",
+        "mod/sub1/sub2/__init__.py",
+        "mod/sub1/sub2/abc.py",
+    ]:
+        (tmpdir / f).touch()
+    return tmpdir
+
+
+@pytest.mark.parametrize(
+    ("path", "expected"),
+    [
+        (".", ["test", "mod.ext"]),
+        ("./", ["test", "mod.ext"]),
+        ("empty/", []),
+    ],
+)
+def test_search_directory(tmp_module_root, path, expected) -> None:
+    orig_cwd = os.getcwd()
+    try:
+        os.chdir(tmp_module_root)
+
+        # test relative and absolute paths
+        for p in [path, os.path.abspath(path)]:
+            assert sorted(utils.search_directory(p)) == sorted(expected)
+    finally:
+        os.chdir(orig_cwd)
+
+
+@pytest.mark.parametrize(
+    ("path", "exc"),
+    [
+        ("../../", r"Modules outside the cwd require a package to be specified"),
+        ("nonexistent", r"Provided path '.*?nonexistent' does not exist"),
+        ("test.py", r"Provided path '.*?test.py' is not a directory"),
+    ],
+)
+def test_search_directory_exc(tmp_module_root, path, exc) -> None:
+    orig_cwd = os.getcwd()
+    try:
+        os.chdir(tmp_module_root)
+
+        with pytest.raises(ValueError, match=exc):
+            list(utils.search_directory(tmp_module_root / path))
+    finally:
+        os.chdir(orig_cwd)
